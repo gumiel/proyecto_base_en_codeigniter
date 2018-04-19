37cc9f7422b3e8ddc6319f12006dbd7afc82d4af
henry perez <hperezcbba@gmail.com>
2018-04-19 18:08:49 -0400

    14741

--- application/controllers/Backup.php
+++ application/controllers/Backup.php
@@ -7,8 +7,11 @@ class Backup extends CI_Controller {
 		parent::__construct();
 		//Do your magic here
 	}
+
 	public function index()
 	{
+		// echo FCPATH;
+		// exit;
 		// Load the DB utility class
 		$this->load->dbutil();
 
@@ -22,6 +25,45 @@ class Backup extends CI_Controller {
 		// Load the download helper and send the file to your desktop
 		$this->load->helper('download');
 		force_download('codeigniter.backupBD.gz', $backup);
+	}
+
+	public function export()
+	{
+		// Load the DB utility class
+		$this->load->dbutil();
+		$this->load->helper('file');
+		$this->load->helper('download');
+
+		$prefs = array(
+	        'format'        => 'txt',                       // gzip, zip, txt
+	        'filename'      => 'mybackup.sql',              // File name - NEEDED ONLY WITH ZIP FILES
+	        'add_drop'      => TRUE,                        // Whether to add DROP TABLE statements to backup file
+	        'add_insert'    => TRUE,                        // Whether to add INSERT data to backup file
+	        'newline'       => "\n"                         // Newline character used in backup file
+		);
+
+		// Backup your entire database and assign it to a variable
+		$backup = $this->dbutil->backup($prefs);
+
+		// Load the file helper and write the file to your server
+		write_file(FCPATH.'codeigniter.backupBD.sql', $backup);
+
+		// Load the download helper and send the file to your desktop
+		force_download('codeigniter.backupBD.sql', $backup);
+	}
+
+	public function import()
+	{
+		if ($this->dbforge->drop_database('codeigniter'))
+		{
+		    echo 'Database deleted!<br>';
+		}
+
+		if ($this->dbforge->create_database('codeigniter'))
+		{
+		    echo 'Database created!<br>';
+		    $this->db->query(file_get_contents(FCPATH."codeigniter.backupBD.sql"));
+		}
 	}
 
 }
