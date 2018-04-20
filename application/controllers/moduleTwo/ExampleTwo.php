8c79e1694c3d20b3e2d36e17e54e3855ad1be3f7
henry perez <hperezcbba@gmail.com>
2018-04-20 11:48:57 -0400

    4234

--- application/controllers/moduleTwo/ExampleTwo.php
+++ application/controllers/moduleTwo/ExampleTwo.php
@@ -3,14 +3,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ExampleTwo extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->library('moduleTwo/ExampleTwoRule');
+	}
+
 	public function index()
 	{
 		// echo "Hello programmer from /moduleTwo/ExampleTwo";
-		
+
 		$data['dataSend'] = "Hello Programmer";
 		$this->load->view('moduleTwo/exampleTwo', $data, FALSE);
 	}
 
+	public function processForm()
+	{
+		
+	}
 }
 
 /* End of file ExampleTwo.php */
