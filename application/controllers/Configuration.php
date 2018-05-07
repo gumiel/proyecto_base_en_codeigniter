12fd0fd80d105e804a97df1b11ef2059f51d18ec
henry perez <hperezcbba@gmail.com>
2018-05-07 17:27:45 -0400

    auditoria 894855

--- application/controllers/Configuration.php
+++ application/controllers/Configuration.php
@@ -3,12 +3,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Configuration extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		
+		//
+		$tablesRequired = array("auditor_query");
+	}
+
 	public function index()
 	{
+
 		$this->load->view('configuration/index');
 	}
 
+	private function verifyTables()
+	{		
+
+		$existingTables = $this->db->list_tables();
+
+		foreach ($tablesRequired as $table)
+		{
+		    if (in_array($table, $existingTables) == false) {
+ 				return false;
+ 				break;
+			}
+
+		}
+
+		return true;
+		
+	}
+
 }
+
+
 
 /* End of file Configuration.php */
 /* Location: ./application/controllers/Configuration.php */
