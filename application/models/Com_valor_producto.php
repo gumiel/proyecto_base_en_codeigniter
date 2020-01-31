c20018fd720b8759c114d3204dd5991528272ded
Henry Perez Gumiel <henry.perez@corcbb.local>
2020-01-31 12:52:16 -0400

    Inicio de compras

--- application/models/Com_valor_producto.php
+++ application/models/Com_valor_producto.php
@@ -10,6 +10,15 @@ class Com_valor_producto extends Generic_Model {
 		$this->foreignKeyName('id_valor_producto');
 	}
 
+	public function listAtributosValoresByIdProducto($idProducto)
+	{
+		$this->db->join('com_valor_producto', 'com_valor_producto.id_producto = com_producto.id_producto', 'inner');
+		$this->db->join('com_atributo_producto', 'com_atributo_producto.id_atributo_producto = com_valor_producto.id_atributo_producto', 'inner');
+		$this->db->where('com_producto.id_producto', $idProducto);
+		$result = $this->db->get('com_producto');
+		return $result->result();
+	}
+
 }
 
 /* End of file Com_valor_producto.php */
