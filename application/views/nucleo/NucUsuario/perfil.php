d8936cbfa147079517305a9b92dfc9910992aac4
Henry Perez Gumiel <henry.perez@corcbb.local>
2020-02-18 15:36:20 -0400

    Modales para el perfil

--- application/views/nucleo/NucUsuario/perfil.php
+++ application/views/nucleo/NucUsuario/perfil.php
@@ -7,26 +7,75 @@ $this->templateci->addJs("public/nucleo/NucUsuario/perfil.js?data=".date('YmdHis
 
 <?php $this->load->view('template/up'); ?>
 
-	<!-- Main content -->	  
-	<?php echo section_open("ctnBuscador", "Datos del usuario"); ?>
+	<!-- Main content -->	
+	<div class="col-md-6">  
+		<?php echo section_open("ctnBuscador", "Cambiar datos del usuario"); ?>
+			<?php echo form_open_multipart_ci('usuario/editUsuario', ['id'=>'formEditarUsuario']); ?>
+				<?php echo form_input_ci("Cuenta", "usuario[cuenta]", $cuenta); ?>
+				<?php echo form_input_ci("Email", "usuario[email]", $email); ?>              
+				<button type="button" class="btn btn-primary">Guardar</button>
+			<?php echo form_close(); ?>
+		<?php echo section_close(); ?>
+	</div>
 
-		<?php echo form_open_multipart_ci('usuario/editUsuario', ['id'=>'formEditarUsuario']); ?>
-			<div class="col-md-6">
-			    
-	              
-                <?php echo form_input_ci("Cuenta", "usuario[cuenta]", $cuenta); ?>
+	<!-- Main content -->	
+	<div class="col-md-6">  
+		<?php echo section_open("ctnBuscador", "Cambiar contraseña"); ?>
+			<?php echo form_open_multipart_ci('usuario/editUsuario', ['id'=>'formEditarUsuario']); ?>                
+				<?php echo form_password_ci("Nueva contraseña", 'usuario[clave]', '', [ 'id'=>'clave']); ?>
+				<?php echo form_password_ci("Repetir nueva contraseña", 'usuario[rep_clave]', ''); ?>             
+				<button type="button" class="btn btn-primary">Guardar</button>
+			<?php echo form_close(); ?>
+		<?php echo section_close(); ?>
+	</div>
 
-                <?php echo form_input_ci("Email", "usuario[email]", $email); ?>              
 
-                <?php echo form_password_ci("Contraseña", "usuario[clave]", ""); ?>
 
-                <?php echo form_password_ci("Repetir Contraseña", "usuario[rep_clave]", ""); ?>
 
-	             <button type="submit" class="btn btn-primary">Guardar</button>
 
-			</div>
-		<?php echo form_close(); ?>
-	<?php echo section_close(); ?>
+
+
+
+
+	<div class="modal fade" id="modalEdit">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <?php echo form_open_multipart_ci('usuario/editUsuario', ['id'=>'formEdit']); ?>
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">Editar Usuario</h4>
+        </div>
+        <div class="modal-body">
+        
+            <div class="box-body">
+              <?php echo form_input_ci("Nombres", 'usuario[nombres]', ""); ?>
+              
+              <?php echo form_input_ci("Apellido Paterno", 'usuario[paterno]', ''); ?>
+              
+              <?php echo form_input_ci("Apellido Materno", 'usuario[materno]', ''); ?>
+              
+              <?php echo form_input_ci("Cuenta", 'usuario[cuenta]', ''); ?>
+
+              <?php echo form_input_ci("Email", 'usuario[email]', ""); ?>
+
+              <?php echo form_input_ci("CI", 'usuario[ci]', ''); ?>
+
+              <?php echo form_password_ci("Contraseña", 'usuario[password]', ''); ?>
+
+              <?php echo form_password_ci("Repetir Contraseña", 'usuario[rep_password]', ''); ?>
+
+            </div>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+          <button type="submit" class="btn btn-primary">Guardar</button>
+          <?php echo form_hidden('usuario[id_usuario]', ''); ?>
+        </div>
+        <?php echo form_close(); ?>
+      </div>
+    </div>
+  </div>
 
 
 <?php $this->load->view('template/down'); ?>
